298c9626120f9e6568597df60dfc682a28efb26e
Mateus Silva <mateusilva.dev@gmail.com>
2025-07-20 14:43:15 -0300

    Detalhes da meal (navigation)

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,40 @@
+import {
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  HostGrotesk_700Bold,
+  useFonts,
+} from '@expo-google-fonts/host-grotesk';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import '../styles/global.css';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    HostGrotesk_400Regular,
+    HostGrotesk_500Medium,
+    HostGrotesk_600SemiBold,
+    HostGrotesk_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <Stack />
+    <SafeAreaProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </SafeAreaProvider>
   );
 }
--- src/components/MealCard.tsx
+++ src/components/MealCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'expo-router';
 import { Text, TouchableOpacity, View } from 'react-native';
 
 interface IMealCardProps {
@@ -5,27 +6,29 @@ interface IMealCardProps {
   name: string;
 }
 
-export function MealCard({ name }: IMealCardProps) {
+export function MealCard({ name, id }: IMealCardProps) {
   return (
-    <TouchableOpacity>
-      <Text className="text-base font-sans-regular text-gray-700">
-        Hoje, 12h25
-      </Text>
+    <Link href={`/meals/${id}`} asChild>
+      <TouchableOpacity>
+        <Text className="text-base font-sans-regular text-gray-700">
+          Hoje, 12h25
+        </Text>
 
-      <View className="mt-2 px-4 py-5 flex-row gap-3 items-center border border-gray-400 rounded-2xl">
-        <View className="size-12 bg-gray-200 rounded-full items-center justify-center">
-          <Text>🔥</Text>
-        </View>
+        <View className="mt-2 px-4 py-5 flex-row gap-3 items-center border border-gray-400 rounded-2xl">
+          <View className="size-12 bg-gray-200 rounded-full items-center justify-center">
+            <Text>🔥</Text>
+          </View>
 
-        <View>
-          <Text className="text-base font-sans-regular text-gray-700">
-            {name}
-          </Text>
-          <Text className="text-base font-sans-medium text-black-700">
-            Pão
-          </Text>
+          <View>
+            <Text className="text-base font-sans-regular text-gray-700">
+              {name}
+            </Text>
+            <Text className="text-base font-sans-medium text-black-700">
+              Pão
+            </Text>
+          </View>
         </View>
-      </View>
-    </TouchableOpacity>
+      </TouchableOpacity>
+    </Link>
   );
 }
