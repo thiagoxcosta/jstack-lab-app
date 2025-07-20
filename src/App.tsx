1a6780927d40a196f8eebf4b2ec0cf6f59a38a7e
Mateus Silva <mateusilva.dev@gmail.com>
2025-07-20 10:34:41 -0300

    Componente HomeHeader

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,11 @@ import {
   useFonts,
 } from '@expo-google-fonts/host-grotesk';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 import { useEffect } from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { HomeHeader } from './components/HomeHeader';
 import './styles/global.css';
 
 SplashScreen.preventAutoHideAsync();
@@ -33,9 +34,10 @@ export default function App() {
   }
 
   return (
-    <View className="bg-lime-500 flex-1 items-center justify-center">
-      <Text className="text-base">Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 bg-white">
+      <SafeAreaProvider>
+        <HomeHeader />
+      </SafeAreaProvider>
     </View>
   );
 }
