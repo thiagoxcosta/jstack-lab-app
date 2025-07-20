b77e5c43acf27c17a50844607769784f7fb0f2f9
Mateus Silva <mateusilva.dev@gmail.com>
2025-07-20 17:54:33 -0300

    React hook form no Signup

--- src/app/(public)/signup.tsx
+++ src/app/(public)/signup.tsx
@@ -1,15 +1,22 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { router } from 'expo-router';
 import { ArrowLeftIcon, ArrowRightIcon } from 'lucide-react-native';
 import { useState } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
 import { View } from 'react-native';
 import { AuthLayout } from '../../components/AuthLayout';
 import { Button } from '../../components/Button';
 import { GenderStep } from '../../components/SignUpSteps/GenderStep';
 import { GoalStep } from '../../components/SignUpSteps/GoalStep';
+import { signUpSchema } from '../../components/SignUpSteps/signUpSchema';
 import { colors } from '../../styles/colors';
 
 export default function SignUp() {
   const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  const form = useForm({
+    resolver: zodResolver(signUpSchema),
+  });
 
   const steps = [
     {
@@ -48,7 +55,9 @@ export default function SignUp() {
       subtitle={currentStep.subtitle}
     >
       <View className="justify-between flex-1">
-        <currentStep.Component />
+        <FormProvider {...form}>
+          <currentStep.Component />
+        </FormProvider>
 
         <View className="flex-row justify-between">
           <Button size="icon" color="gray" onPress={handlePreviousStep}>
