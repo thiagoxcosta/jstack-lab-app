f7ff721a65f086682b3acde1d77eb8a004a566f2
Mateus Silva <mateusilva.dev@gmail.com>
2025-07-20 09:52:07 -0300

    Setup tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+import './styles/global.css';
+
+export default function App() {
+  return (
+    <View className="bg-lime-500 flex-1 items-center justify-center">
+      <Text className="text-base">Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
