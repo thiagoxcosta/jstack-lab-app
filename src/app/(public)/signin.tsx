08f3e4a6e41d56e60a6a186f6f06f060e984d052
Mateus Silva <mateusilva.dev@gmail.com>
2025-07-20 16:52:45 -0300

    Conecta react hook form ao signin

--- src/app/(public)/signin.tsx
+++ src/app/(public)/signin.tsx
@@ -1,13 +1,34 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { router } from 'expo-router';
 import { ArrowLeftIcon } from 'lucide-react-native';
 import React from 'react';
+import { Controller, useForm } from 'react-hook-form';
 import { View } from 'react-native';
+import z from 'zod';
+
 import { AuthLayout } from '../../components/AuthLayout';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import { colors } from '../../styles/colors';
 
+const schema = z.object({
+  email: z.email('Informe um e-mail válido'),
+  password: z.string().min(8, 'Deve conter pelo menos 8 caracteres'),
+});
+
 export default function SignIn() {
+  const form = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const handleSubmit = form.handleSubmit((formData) => {
+    console.log(JSON.stringify(formData, null, 2));
+  });
+
   return (
     <AuthLayout
       icon="👤"
@@ -16,20 +37,38 @@ export default function SignIn() {
     >
       <View className="justify-between flex-1">
         <View className="gap-6">
-          <Input
-            label="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
-            autoCorrect={false}
-            autoComplete="email"
+          <Controller
+            control={form.control}
+            name="email"
+            render={({ field, fieldState }) => (
+              <Input
+                label="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+                autoComplete="email"
+                value={field.value}
+                onChangeText={field.onChange}
+                error={fieldState.error?.message}
+              />
+            )}
           />
         
-          <Input
-            label="Senha"
-            autoCapitalize="none"
-            autoCorrect={false}
-            autoComplete="password"
-            secureTextEntry
+          <Controller
+            control={form.control}
+            name="password"
+            render={({ field, fieldState }) => (
+              <Input
+                label="Senha"
+                autoCapitalize="none"
+                autoCorrect={false}
+                autoComplete="password"
+                secureTextEntry
+                value={field.value}
+                onChangeText={field.onChange}
+                error={fieldState.error?.message}
+              />
+            )}
           />
         </View>
 
@@ -37,7 +76,9 @@ export default function SignIn() {
           <Button onPress={router.back} size="icon" color="gray">
             <ArrowLeftIcon size={20} color={colors.black[700]} />
           </Button>
-          <Button className="flex-1">Entrar</Button>
+          <Button className="flex-1" onPress={handleSubmit}>
+            Entrar
+          </Button>
         </View>
       </View>
     </AuthLayout>
